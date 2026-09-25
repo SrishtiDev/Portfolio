e79934f18e912e26903193f1e35e88d8934f90b8
Srishti Rawat <rawat.srishti1010@gmail.com>
2026-09-25 17:03:30 +0530

    Add next.config.js configuration file

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
